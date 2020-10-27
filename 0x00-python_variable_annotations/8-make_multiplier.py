7ba20adecfb4001f7e1eb6426676d0d8766be2bd
omeinsotelo <omeinsotelo@gmail.com>
2020-10-27 13:48:47 -0500

    upload files

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-Complex types - functions 
+Complex types - functions
 """
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
-    Returns 
+    Returns multipier of two numbers
     """
     return lambda x: x * multiplier
