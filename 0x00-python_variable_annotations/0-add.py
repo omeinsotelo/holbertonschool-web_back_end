d396842dbb0abde8b3d7c0622bf730ef03e84d99
omeinsotelo <omeinsotelo@gmail.com>
2020-10-27 12:02:21 -0500

    upload files

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function add
+"""
+def add(a: float, b: float) ->float:
+    """
+    Add a + b
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function concat
+"""
+def concat(str1: str, str2: str) ->str:
+    """
+    concat str1 + str2
+    """
+    return str1 + str2
