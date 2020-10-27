0cc9b38f367bfea3836dd318bf1e386a6561fbe9
omeinsotelo <omeinsotelo@gmail.com>
2020-10-27 13:46:45 -0500

    upload files

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Complex types - functions 
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns 
+    """
+    return lambda x: x * multiplier
