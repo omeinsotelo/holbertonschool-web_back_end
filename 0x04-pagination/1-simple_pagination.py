f1d6a25a317c4382725ac11bc89409e85fe40135
omeinsotelo <omeinsotelo@gmail.com>
2020-10-28 18:29:03 -0500

    upload files

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Simple pagination
+"""
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """
+    Server class to paginate a database
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """
+        return a tuple of size two containing a start index and an end index
+        """
+        end: int = page * page_size
+        start: int = 0
+        for _ in range(page - 1):
+            start += page_size
+        return (start, end)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        get page
+        """
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+        dataset = self.dataset()
+        start, end = self.index_range(page, page_size)
+        if end > len(dataset):
+            return []
+        return [list(dataset[row]) for row in range(start, end)]
