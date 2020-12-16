08933c88b56330d4629576f4fd44132425064857
omeinsotelo <omeinsotelo@gmail.com>
2020-12-15 20:52:32 -0500

    upload

--- 0x0D-NoSQL/102-log_stats.py
+++ 0x0D-NoSQL/102-log_stats.py
@@ -1,34 +1,34 @@
-#!/usr/bin/env python3
-"""
-Where can I learn Python?
-"""
-from pymongo import MongoClient
+#! / usr / bin / env python3
+"" "
+¿Dónde puedo aprender Python?
+"" "
+desde  pymongo  importar  MongoClient
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
-    number = nginx_collection.count()
-    number_get = nginx_collection.find({"method": "GET"}).count()
-    number_post = nginx_collection.find({"method": "POST"}).count()
-    number_put = nginx_collection.find({"method": "PUT"}).count()
-    number_patch = nginx_collection.find({"method": "PATCH"}).count()
-    number_delete = nginx_collection.find({"method": "DELETE"}).count()
-    number_status = nginx_collection.find(
-        {"method": "GET", "path": "/status"}).count()
-    number_ips = nginx_collection.aggregate([
-            {"$group": {"_id": "$ip", "total": {"$sum": 1}}},
-            {"$sort": {"total": -1}},
-            {"$limit": 10}
+if  __name__  ==  "__main__" :
+    cliente  =  MongoClient ( 'mongodb: //127.0.0.1: 27017' )
+    nginx_collection  =  cliente . registros . nginx
+    número  =  colección_nginx . contar ()
+    number_get  =  nginx_collection . buscar ({ "método" : "OBTENER" }). contar ()
+    número_post  =  nginx_collection . buscar ({ "método" : "POST" }). contar ()
+    number_put  =  nginx_collection . encontrar ({ "método" : "PUT" }). contar ()
+    parche_número  =  colección_nginx . buscar ({ "método" : "PATCH" }). contar ()
+    number_delete  =  nginx_collection . buscar ({ "método" : "BORRAR" }). contar ()
+    number_status  =  nginx_collection . encontrar (
+        { "método" : "OBTENER" , "ruta" : "/ estado" }). contar ()
+    number_ips  =  nginx_collection . agregado ([
+            { "$ grupo" : { "_id" : "$ ip" , "total" : { "$ suma" : 1 }}},
+            { "$ sort" : { "total" : - 1 }},
+            { "$ límite" : 10 }
         ])
 
-    print("{} logs".format(number))
-    print("Methods:")
-    print("\tmethod GET: {}".format(number_get))
-    print("\tmethod POST: {}".format(number_post))
-    print("\tmethod PUT: {}".format(number_put))
-    print("\tmethod PATCH: {}".format(number_patch))
-    print("\tmethod DELETE: {}".format(number_delete))
-    print("{} status check".format(number_status))
-    print("IPs:")
-    for ips in number_ips:
-        print("\t{}: {}".format(ips.get("_id"), ips.get("total")))
+    imprimir ( "{} registros" . formato ( número ))
+    print ( "Métodos:" )
+    print ( " \ t método GET: {}" . formato ( number_get ))
+    print ( " \ t método POST: {}" . formato ( número_post ))
+    print ( " \ t método PUT: {}" . formato ( number_put ))
+    print ( " \ t método PATCH: {}" . formato ( number_patch ))
+    print ( " \ t método DELETE: {}" . formato ( number_delete ))
+    print ( "{} verificación de estado" . formato ( number_status ))
+    imprimir ( "direcciones IP:" )
+    para  ips  en  number_ips :
+        print ( " \ t {}: {}" . formato ( ips . get ( "_id" ), ips . get ( "total" )))
