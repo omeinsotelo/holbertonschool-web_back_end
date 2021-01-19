411b55d3189c9f674f915f8708ff144ecc614153
omeinsotelo <omeinsotelo@gmail.com>
2021-01-19 16:35:33 -0500

    upload folders fix

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -1,14 +1,14 @@
 export class HolbertonClass {
-  constructor (year, location) {
+  constructor(year, location) {
     this._year = year;
     this._location = location;
   }
 
-  get year () {
+  get year() {
     return this._year;
   }
 
-  get location () {
+  get location() {
     return this._location;
   }
 }
@@ -17,13 +17,13 @@ const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-  constructor (firstName, lastName, holbertonClass) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
   }
 
-  get fullName () {
+  get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
 
