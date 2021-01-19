8ac2f021c9bd6330cb79d7e103c364ffd9ff4dde
omeinsotelo <omeinsotelo@gmail.com>
2021-01-19 15:40:01 -0500

    upload folders fix

--- 0x0F-ES6_promise/0-promise.js
+++ 0x0F-ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
 /* promise need a function to work */
-export default function getResponseFromAPI () {
+export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => [resolve, reject]);
 }
